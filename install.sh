b30dd744f610a6132459172f3a2668588ab31838
Jake <47207140+xjakesl@users.noreply.github.com>
2021-05-25 09:20:16 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,6 @@ apt update && apt upgrade -y
 apt install python3.8 pip3 ffmpeg npm apache2 libapache2-mod-wsgi-py3
 a2enmod wsgi
 pip3 install -r requirements.txt
-pip3 install ffmpeg-python
 npm install pm2 -g
 
 
